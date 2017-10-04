27c32f3bd25559b36f11c1ab5c125984fc481eb3
Gregory Dukes <kcils360@gmail.com>
2017-10-04 11:59:36 -0700

    fixing git

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -8,12 +8,12 @@ namespace BinaryTree
         {
             Node root = new Node(1);
             Tree tree = new Tree(root);
-            root.right = new Node(2);
-            root.left = new Node(3);
-            root.right.right = new Node(4);
-            root.right.left = new Node(5);
-            root.left.left = new Node(6);
-            root.left.right = new Node(7);
+            //root.right = new Node(2);
+            //root.left = new Node(3);
+            //root.right.right = new Node(4);
+            //root.right.left = new Node(5);
+            //root.left.left = new Node(6);
+            //root.left.right = new Node(7);
 
             string startString = "";
             tree.PrintTree(root, ref startString);
